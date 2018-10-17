dcc7c615685da21118e2a7884c703f566fd4ea90
dataisland98 <dataisland@outlook.com>
2018-10-17 13:06:49 +0800

    add mit licence

--- matrix.hpp
+++ matrix.hpp
@@ -1,9 +1,29 @@
-#ifndef SJTU_MATRIX_HPP
-#define SJTU_MATRIX_HPP
+// Copyright (c) 2018 dataisland
+ 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+ 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+ 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
 
 #include <bits/stdc++.h>
 
-namespace sjtu{
+namespace matrix{
 #define ASSERT(expr, message) if (!(expr)) { throw message; }
   template <typename T>
   class Matrix{
@@ -243,4 +263,8 @@ namespace sjtu{
   };
 }
 
-#endif //SJTU_MATRIX_HPP
+namespace sjtu{
+  using namespace matrix;
+}
+
+#endif //MATRIX_HPP
